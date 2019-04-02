2df4c1719c26ed19a15d42e44b359a779f3f230a
raj0nh0ssain <41703420+raj0nh0ssain@users.noreply.github.com>
2019-04-02 12:48:39 +0600

    initial comment

---
+++ js/fj.js
@@ -0,0 +1,58 @@
+particlesJS(
+  "dots", {
+  "particles": {
+    "number": {
+      "value": 180
+    },
+    "color": {
+      "value": "#F3AE65"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": true
+      }
+    },
+    "size": {
+      "value": 8,
+      "random": true,
+      "anim": {
+        "enable": true,
+      }
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 4,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "push": {
+        "particles_nb": 10
+      }
+    }
+  },
+  "retina_detect": true
+});
